51b27127c196b67e67424eeb10bf213b2c69246d
Luis Gaudencio <luis_g24@live.com>
2023-05-23 21:35:02 +0000

    Fix typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,7 +75,7 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
-        return [operand1 + operand2, "multiply"]
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
